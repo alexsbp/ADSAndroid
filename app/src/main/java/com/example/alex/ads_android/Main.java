28029cdf218842bb5d703c5b2c57690c0f73852a
alexsbp <alex1546@student.eadania.dk>
2018-03-20 10:01:38 +0100

    click button to go to new window

--- app/src/main/java/com/example/alex/ads_android/Main.java
+++ app/src/main/java/com/example/alex/ads_android/Main.java
@@ -1,15 +1,37 @@
 package com.example.alex.ads_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToNewWindow();
+
     }
+
+    public void ToNewWindow()
+    {
+        b1 = (Button)findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(Main.this, Main2Activity.class));
+            }
+        });
+    }
+
 
 
 }
---
+++ app/src/main/java/com/example/alex/ads_android/Main2Activity.java
@@ -0,0 +1,37 @@
+package com.example.alex.ads_android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Button b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ToOldWindow();
+    }
+
+    public void ToOldWindow()
+    {
+        b2 = (Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(Main2Activity.this, Main.class));
+
+
+                /*Intent intent = new Intent(getApplicationContext(), Main.class);
+                startActivity(intent);*/
+            }
+        });
+    }
+}
